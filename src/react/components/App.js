809eaf33fc87d2157f01eb92e431088f0ae5fb75
srujan <srujanakshanthala@gmail.com>
2019-10-21 23:28:52 +0530

    first-commit

---
+++ src/react/components/App.js
@@ -0,0 +1,19 @@
+/*globals COURSES:true */
+import React, { Component } from 'react'
+// import Dashboard from './Dashboard'
+// import GlobalNav from './GlobalNav'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        {/* <GlobalNav /> */}
+        <div className="container">
+         <p className="lead">jkdfkdsgjvvvvvgjewjewj</p>
+        </div>
+      </div>
+    )
+  }
+}
+
+module.exports = App
